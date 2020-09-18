64d860f6464fcb4556905b76d773d64f0a89a5fa
Salo <salomonco@gmail.com>
2020-09-18 14:16:02 -0400

    math works

--- code.js
+++ code.js
@@ -1,9 +1,13 @@
-let smallMass =
-let bigMass = 
-let smallVelocity = 
-let bigVelocity = 
+let mass1 = 1
+let velocity1 = 10
+let mass2 = 1
+let velocity2 = 10
 
-
+function Collision(m1,v1,m2,v2) {
+    vF = ((m1 * v1) + (m2 * v2)) / (m1 + m2)
+    return vF
+}
+ console.log(Collision())
 
 
 
